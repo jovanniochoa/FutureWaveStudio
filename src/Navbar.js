8d61219230cc76e4a33222db67d3e85e12ab1913
jovanniochoa <57188689+jovanniochoa@users.noreply.github.com>
2023-10-17 18:56:51 -0500

    Added Navbar
    Added Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,39 +1,50 @@
-import React from 'react';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isAboutDropdownOpen, setIsAboutDropdownOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const toggleAboutDropdown = () => {
+    setIsAboutDropdownOpen(!isAboutDropdownOpen);
+  };
+
   return (
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="./futurewavestudio_logo.jpg" alt="Company Logo" />
+        <h1>FutureWave Studio</h1>
       </div>
-      <ul className="app__navbar-links">
-        <li className="p__opensans"><a href="#home">Home</a></li>
-        <li className="p__opensans"><a href="#about">About</a></li>
-        <li className="p__opensans"><a href="#menu">Menu</a></li>
-        <li className="p__opensans"><a href="#awards">Awards</a></li>
-        <li className="p__opensans"><a href="#contact">Contact</a></li>
-      </ul>
-      <div className="app__navbar-login">
-        <a href="#login" className="p__opensans">Log In / Registration</a>
-        <div />
-        <a href="/" className="p__opensans">Book Table</a>
+      <div
+        className={`navbar-toggle ${isMobileMenuOpen ? 'open' : ''}`}
+        onClick={toggleMobileMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-      <div className="app__navbar-smallscreen">
-        {toggleMenu && (
-          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
-            <ul className="app__navbar-smallscreen_links">
-              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
-              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
-              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
-              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
-              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+      <ul className={`navbar-links ${isMobileMenuOpen ? 'active' : ''}`}>
+        <li><a href="#home">Home</a></li>
+        <li
+          className={`dropdown ${isAboutDropdownOpen ? 'open' : ''}`}
+          onMouseEnter={toggleAboutDropdown}
+          onMouseLeave={toggleAboutDropdown}
+        >
+          <a href="#apps">Apps</a>
+          <div className="dropdown-menu">
+            <ul>
+              <li><a href="#reaction">Reaction</a></li>
+              <li><a href="#rock_climbing">Rock Climbing</a></li>
             </ul>
           </div>
-        )}
-      </div>
+        </li>
+        <li><a href="#support">Support</a></li>
+        <li><a href="#about_us">About Us</a></li>
+      </ul>
     </nav>
   );
 };
