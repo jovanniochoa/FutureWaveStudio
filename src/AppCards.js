c3c4c631d325848e0b08dbe5993217bf68f3359a
jovanniochoa <57188689+jovanniochoa@users.noreply.github.com>
2023-10-18 21:48:37 -0500

    Fixed Cards
    Fixed Cards

--- src/AppCards.js
+++ src/AppCards.js
@@ -5,15 +5,15 @@ const AppCards = () => {
   return (
     <div className="app-cards">
       <div className="card">
-        <img src="your_image_url" alt="Image 1" />
-        <h2>Card Title 1</h2>
-        <p>Description for Card 1.</p>
+        <img src="./ClimbingAppLogo.png" alt="Climbing App Logo" />
+        <h2>Ascent Mastery</h2>
+        <p>Get ready for a thrilling rock climbing adventure like no other. Ascent Mastery is not just an app; it's your passport to the ultimate climbing experience. Picture this: a fusion of rock climbing and gaming, where the thrill of ascending challenging routes goes hand-in-hand with competing against climbers from around the world.</p>
       </div>
 
       <div className="card">
-        <img src="your_image_url" alt="Image 2" />
-        <h2>Card Title 2</h2>
-        <p>Description for Card 2.</p>
+        <img src="./ReactionTimeLogo.png" alt="Climbing App Logo" />
+        <h2>ReactMaster</h2>
+        <p>Ready to challenge your reflexes and see how quick you really are? Look no further! ReactMaster is the ultimate reaction speed app that will put your skills to the test and keep you coming back for more.</p>
       </div>
 
       {/* Add more cards as needed */}
